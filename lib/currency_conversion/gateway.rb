4ef39d4fe28f3cee35b9f8e2617bbc0258ff83e7
Kamal Ejaz <a2ninek@yahoo.com>
2013-04-29 15:21:03 +0500

    Bug fixes

--- lib/currency_conversion/gateway.rb
+++ lib/currency_conversion/gateway.rb
@@ -12,10 +12,10 @@ module CurrencyConversion
 
       if response_hash['error'].nil? or response_hash['error'] == ''
         encoding_options = {
-          :invalid           => :replace,  # Replace invalid byte sequences
-          :undef             => :replace,  # Replace anything not defined in ASCII
-          :replace           => '',        # Use a blank for those replacements
-          :universal_newline => true       # Always break lines with \n
+          :invalid           => :replace,  
+          :undef             => :replace, 
+          :replace           => '',      
+          :universal_newline => true    
         }
         response_hash['rhs'].encode(Encoding.find('ASCII'), encoding_options).to_f
       else
