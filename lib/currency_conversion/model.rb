10769ee86ad37a0ad9daa14148bf7dfe372bb718
Kamal Ejaz <a2ninek@yahoo.com>
2013-05-03 15:23:59 +0500

    Bug fixes

--- lib/currency_conversion/model.rb
+++ lib/currency_conversion/model.rb
@@ -3,9 +3,9 @@ module CurrencyConversion
   module Model
     def conversion_fields(*fields)
       fields.each do |field|
-        define_method "#{field.to_s}_to" do |args, from=""|
-          return self.send(field) if args.nil? || ( args == "usd" && !from )
-          @gateway = CurrencyConversion::Gateway.new value: self.send(field), from: from || "usd", to: args
+        define_method "#{field.to_s}_to" do |args, from="usd"|
+          return self.send(field) if args.nil? || ( args == "usd" && from == "usd"  )
+          @gateway = CurrencyConversion::Gateway.new value: self.send(field), from: from , to: args
           @gateway.convert_payment
         end
       end
