50f1270c9711aab3c18007517132bb8981a18b3a
Kamal Ejaz <a2ninek@yahoo.com>
2013-05-03 12:54:29 +0500

    user can give from field

--- lib/currency_conversion/model.rb
+++ lib/currency_conversion/model.rb
@@ -3,9 +3,9 @@ module CurrencyConversion
   module Model
     def conversion_fields(*fields)
       fields.each do |field|
-        define_method "#{field.to_s}_to" do |args|
+        define_method "#{field.to_s}_to" do |args, from=""|
           return self.send(field) if args.nil? || args == "usd"
-          @gateway = CurrencyConversion::Gateway.new value: self.send(field), from: "usd", to: args
+          @gateway = CurrencyConversion::Gateway.new value: self.send(field), from: from || "usd", to: args
           @gateway.convert_payment
         end
       end
